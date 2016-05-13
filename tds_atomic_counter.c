e5a33ad30401f3da81a8efc8a6f2f98474e93a65
lake4790k <lake4790k@users.noreply.github.com>
2016-05-13 20:45:29 +0200

    AtomicCounter + HAS_TORCH

--- tds_atomic_counter.c
+++ tds_atomic_counter.c
@@ -1,6 +1,13 @@
 #include "tds_atomic_counter.h"
 #include "tds_utils.h"
 
+#if HAS_TORCH
+#include "THAtomic.h"
+#else
+#include <pthread.h>
+static pthread_mutex_t ptm = PTHREAD_MUTEX_INITIALIZER;
+#endif
+
 struct tds_atomic_counter_ {
   long count;
   int refcount;
@@ -16,25 +23,62 @@ tds_atomic_counter* tds_atomic_new(void) {
 }
 
 long tds_atomic_inc(tds_atomic_counter *atomic) {
+#if HAS_TORCH
   return THAtomicAddLong(&atomic->count, 1);
+#else
+  pthread_mutex_lock(&ptm);
+  long lastValue = atomic->count;
+  atomic->count++;
+  pthread_mutex_unlock(&ptm);
+  return lastValue;
+#endif
 }
 
 long tds_atomic_get(tds_atomic_counter *atomic) {
+#if HAS_TORCH
   return THAtomicGetLong(&atomic->count);
+#else
+  pthread_mutex_lock(&ptm);
+  long value = atomic->count;
+  pthread_mutex_unlock(&ptm);
+  return value;
+#endif
 }
 
 void tds_atomic_set(tds_atomic_counter *atomic, long value) {
+#if HAS_TORCH
   THAtomicSetLong(&atomic->count, value);
+#else
+  pthread_mutex_lock(&ptm);
+  atomic->count = value;
+  pthread_mutex_unlock(&ptm);
+#endif
 }
 
 void tds_atomic_retain(tds_atomic_counter *atomic) {
+#if HAS_TORCH
   THAtomicIncrementRef(&atomic->refcount);
+#else
+  pthread_mutex_lock(&ptm);
+  atomic->refcount++;
+  pthread_mutex_unlock(&ptm);
+#endif
 }
 
 void tds_atomic_free(tds_atomic_counter *atomic) {
+#if HAS_TORCH
   if(THAtomicDecrementRef(&atomic->refcount))
   {
     tds_free(atomic);
   }
+#else
+  pthread_mutex_lock(&ptm);
+  atomic->refcount--;
+  if(atomic->refcount == 0)
+  {
+    tds_free(atomic);
+  }
+  pthread_mutex_unlock(&ptm);
+#endif
 }
 
